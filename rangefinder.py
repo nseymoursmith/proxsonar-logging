76f1eb855ca04eea6f7964b7651d233ae28e6511
nseymoursmith <nseymoursmith@gmail.com>
2014-08-14 17:50:21 +0100

    Updates SQL database

--- rangefinder.py
+++ rangefinder.py
@@ -8,6 +8,7 @@ import serial
 import glob
 import time
 import logging
+import MySQLdb
 import settings
 
 class rangefinder(object):
@@ -16,14 +17,38 @@ class rangefinder(object):
         self.debug = False
         self.status = 0;
 
-        #Logging initialisation:
-        date = "".join(time.strftime("%x").split("/"))
-        clock = "".join(time.strftime("%X").split(":"))
-        logname = "rangefinderlog_%s_%s.log" % (date, clock)
-        logging.basicConfig(filename = logname, 
-                            format='%(levelname)s:%(message)s', 
-                            level=logging.DEBUG)
-        print "Logging data to file %s. \"Ctrl-c\" to stop" % (logname,)
+        #DB initialisation
+        self.db = MySQLdb.connect(
+            db = settings.DATABASE,
+            host = settings.HOST,
+            user = settings.USER,
+            passwd = settings.PASS
+        )
+
+        self.cur = self.db.cursor()
+        self.cur.execute("SELECT VERSION()")
+        ver = self.cur.fetchone()
+        print "Server version: ", ver[0]
+        
+        self.sqlUpdate = '''
+        UPDATE mccglc_seebri_motiondetect
+        SET active = (%s)
+        WHERE id = (%s)
+        '''
+        self.sqlRead = '''
+        SELECT active 
+        FROM mccglc_seebri_motiondetect
+        WHERE id = (%s)
+        '''
+
+        if settings.LOG:
+            date = "".join(time.strftime("%x").split("/"))
+            clock = "".join(time.strftime("%X").split(":"))
+            logname = "rangefinderlog_%s_%s.log" % (date, clock)
+            logging.basicConfig(filename = logname, 
+                                format='%(levelname)s:%(message)s', 
+                                level=logging.DEBUG)
+            print "Logging data to file %s." % (logname,)
 
         #Open serial port:
         if settings.WINDOWS:
@@ -46,11 +71,12 @@ class rangefinder(object):
                 logging.exception("Error opening serial port for rangefinder:\
                               %s" % (port,))
                 raise
-        logging.info("Serial initialised to port: %s \n\
+        if settings.LOG:
+            logging.info("Serial initialised to port: %s \n\
 Threshold set to: %s inches\nSample taken every %s seconds\n" % 
                      (port,settings.THRESHOLD,settings.RATE))
         print ("Serial initialised to port: %s \n\
-Threshold set to: %s inches\nSample taken every %s seconds\n" % 
+Threshold set to: %s inches\nSample taken every %s seconds\nPress \"Ctrl-c\" to quit" % 
         (port,settings.THRESHOLD,settings.RATE))
 
     def get_message(self, timeout = 10): #timeout in seconds
@@ -66,36 +92,31 @@ Threshold set to: %s inches\nSample taken every %s seconds\n" %
             print "Received line :", received_line
         return received_line
 
-    def interpret(self, received_line):
-        range, status = received_line.split(" ")
-        range = range[1:] #All but first character (R)
-        status = status[1:-1] #All but first (P) and
-                              #last character (carriage return)
-        if self.debug:
-            print "distance: " + range + ", in-range: " + status + "\n"
-        return range, status
-
     def stop(self):
         self.status = 0
 
     def record(self):
         #Start recording:
         self.status = 1
-        start = round(time.time(), 0)
         while self.status:
             try: 
+                self.cur.execute(self.sqlRead, (settings.COLUMN_ID,))
+                dbValue = self.cur.fetchone()
                 received_line = self.get_message()[:-1] #strip \cr
                 temp = received_line.split(" ")
                 distance = int(temp[0][1:])
+                activeValue = int(temp[1][1:])
                 if distance > settings.THRESHOLD:
                     logging.info(received_line)
+                    self.cur.execute(self.sqlUpdate, (activeValue, settings.COLUMN_ID))
                 if not settings.SILENT:
-                    print received_line + "\n"
+                    print "activeValue on database: ", (dbValue[0],)
+                    print "sensor data: " + received_line + "\n"
                 time.sleep(settings.RATE)
             except (KeyboardInterrupt, SystemExit):
+                self.cur.close()
+                self.db.close()
                 raise
-            except:
-                logging.exception("Exception encountered while recording")
 
 if __name__ == "__main__":
     rangefinder = rangefinder().record()
--- settings.py
+++ settings.py
@@ -1,16 +1,30 @@
-# CONFIGURABLE VALUES
+# CONFIGURABLE VALUES:
 
 THRESHOLD = 10 #Minimum distance (inches) included in log 
 
 RATE = 1 #Seconds between readings
 
-SILENT = 0 #If not set, readings will be printed to terminal
+SILENT = 0 #If set, readings will not be printed to terminal
 
-WINDOWS = True #Set to "False" if in Mac/Linux
+LOG = 0 #If set, readings will be written to logfile
 
 PORT = "/dev/tty*USB*" #Regex used to identify serial port in Mac/linux
                        #You can replace this with specific name if you
                        #have multiple serial devices
 
+WINDOWS = True #Set to "False" if in Mac/Linux
+
 WINPORT = "COM1" #Serial port of the rangefinder in windows
 
+
+#DATABASE VARIABLES:
+
+DATABASE = "test" 
+
+HOST = "localhost"
+
+USER = "root"
+
+PASS = "root"
+
+COLUMN_ID = 2 
